9efdc939128daf8391b6b040b038f24a34b60786
ligia.bozzi@gmail.com <ligia@respondeai.com.br>
2022-01-05 11:12:56 -0300

    preparing review

--- src/services/repoprovas.services.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const URL = 'http://localhost:4000/';
-
-const getCategories = () => axios.get(`${URL}categories`);
-
-const getProfessors = () => axios.get(`${URL}professors`);
-
-export { getCategories, getProfessors };
